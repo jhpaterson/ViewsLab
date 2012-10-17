f9da9f1bbb168bda681c315d395fda28aac8a7d8
Jim Paterson <James.Paterson@gcu.ac.uk>
2012-10-17 23:25:34 +0100

    initial commit

---
+++ ViewsLab/ViewsLab/Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using ViewsLab.Domain;
+using ViewsLab.Models;
+
+namespace ViewsLab.Controllers
+{
+    public class UserController : Controller
+    {
+        IUserRepository repository;
+
+        public UserController(IUserRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        //
+        // GET: /User/
+
+        public ActionResult Index(string pageSize, string page, string sort)
+        {
+            // TODO: complete this method
+            return View();
+        }
+
+        public ActionResult UserGrid(string pageSize, string page, string sort)
+        {
+            // TODO: complete this method
+
+            return PartialView();
+        }
+
+        public ActionResult UserDetail(string username)
+        {
+            ViewBag.Username = username;
+            return View();
+        }
+  
+    }
+}
